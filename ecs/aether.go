a0de7a200226951030bc10b4fd76794a11a36692
root <levavakian@gmail.com>
2018-10-26 08:57:40 +0000

    initial ecs framework

---
+++ ecs/aether.go
@@ -0,0 +1,74 @@
+package ecs
+
+import (
+	"errors"
+	"reflect"
+)
+
+// Aether centralized storage for all components
+type Aether struct {
+	Maps map[interface{}]map[EntityId]interface{}
+}
+
+// NewAether creates a new aether
+func NewAether() *Aether {
+	return &Aether{Maps: make(map[interface{}]map[EntityId]interface{})}
+}
+
+// Register adds a component to the aether registry
+func (ae *Aether) Register(id EntityId, cp interface{}) error {
+	if reflect.ValueOf(cp).Kind() != reflect.Ptr {
+		return errors.New("Aether.RegisterComponent: registered type must be pointer")
+	}
+
+	cpType := reflect.TypeOf(cp)
+
+	if _, exists := ae.Maps[cpType]; !exists {
+		ae.Maps[cpType] = make(map[EntityId]interface{})
+	}
+
+	ae.Maps[cpType][id] = cp
+	return nil
+}
+
+// Retrieve retrieves a component based on type and entity id
+func (ae *Aether) Retrieve(id EntityId, cpType reflect.Type) (interface{}, error) {
+	if _, exists := ae.Maps[cpType]; !exists {
+		return nil, errors.New("Aether.Retrieve: type not found")
+	}
+
+	if _, exists := ae.Maps[cpType][id]; !exists {
+		return nil, errors.New("Aether.Retrieve: component not found for entity id")
+	}
+
+	return ae.Maps[cpType][id], nil
+}
+
+func (ae *Aether) RetrieveType(cpType reflect.Type) (map[EntityId]interface{}, error) {
+	if _, exists := ae.Maps[cpType]; !exists {
+		return nil, errors.New("Aether.RetrieveType: type not found")
+	}
+
+	return ae.Maps[cpType], nil
+}
+
+// Deregister deregisters a component based on type and entity id
+func (ae *Aether) DeregisterComponent(id EntityId, cpType reflect.Type) bool {
+	if _, exists := ae.Maps[cpType]; !exists {
+		return false
+	}
+
+	if _, exists := ae.Maps[cpType][id]; !exists {
+		return false
+	}
+
+	delete(ae.Maps[cpType], id)
+	return true
+}
+
+// DeregisterAll deregisters all components for a given id
+func (ae *Aether) DeregisterAll(id EntityId) {
+	for _, v := range ae.Maps {
+		delete(v, id)
+	}
+}
--- main.go
+++ main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "time"
-    "net/http"
-    "fmt"
-    "github.com/gorilla/websocket"
-    "aragno/io"
+	"aragno/io"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"time"
 )
 
 func gameLoop(collector *io.CollectedInputs) {
-    fmt.Println("Starting game loop")
+	fmt.Println("Starting game loop")
 
-    // Set up output dispatcher
-    outputDispatch := io.OutputDispatch{OutputChans: make(map[*websocket.Conn]chan io.StateOutput)}
+	// Set up output dispatcher
+	outputDispatch := io.OutputDispatch{OutputChans: make(map[*websocket.Conn]chan io.StateOutput)}
 
-    // Game loop
-    for {
-        // Collect and consume inputs
-        collectedInputs := collector.Pop()
-        outputDispatch.ConsumeInputs(collectedInputs)
+	// Game loop
+	for {
+		// Collect and consume inputs
+		collectedInputs := collector.Pop()
+		outputDispatch.ConsumeInputs(collectedInputs)
 
-        // Do game stuff
-        state := io.StateOutput{Body: io.BodyState{X: 1, Y: 1, Theta: 1}, Legs: []io.LegState{io.LegState{X: 2, Y: 2, Theta: 2}, io.LegState{X: 3, Y: 3, Theta: 3}}}
+		// Do game stuff
+		state := io.StateOutput{Body: io.BodyState{X: 1, Y: 1, Theta: 1}, Legs: []io.LegState{io.LegState{X: 2, Y: 2, Theta: 2}, io.LegState{X: 3, Y: 3, Theta: 3}}}
 
-        // Output game state to websockets
-        outputDispatch.Dispatch(state)
+		// Output game state to websockets
+		outputDispatch.Dispatch(state)
 
-        // Server run rate at ~30Hz
-        time.Sleep(time.Millisecond * 33)
-    }
+		// Server run rate at ~30Hz
+		time.Sleep(time.Millisecond * 33)
+	}
 }
 
 func main() {
-    // Set up collecter to grab outputs
-    collector := &io.CollectedInputs{InputChan: make(chan io.PlayerInput), Inputs: make(map[*websocket.Conn]io.PlayerInput), CloseSig: make(chan struct{})}
-    go collector.Collect()
+	// Set up collecter to grab outputs
+	collector := &io.CollectedInputs{InputChan: make(chan io.PlayerInput), Inputs: make(map[*websocket.Conn]io.PlayerInput), CloseSig: make(chan struct{})}
+	go collector.Collect()
 
-    go gameLoop(collector)
+	go gameLoop(collector)
 
-    http.HandleFunc("/connect", io.Connect(collector.InputChan))
-    fmt.Println(http.ListenAndServe("localhost:8000", nil))
+	http.HandleFunc("/connect", io.Connect(collector.InputChan))
+	fmt.Println(http.ListenAndServe("localhost:8000", nil))
 }
