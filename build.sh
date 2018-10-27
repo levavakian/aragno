99b62cb3bdf6f07c0e00b4e2bb45e4e64c8c8f1a
Carl Saldanha <cjds@users.noreply.github.com>
2018-10-27 12:28:48 -0700

    npm (#1)

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-docker build --build-arg GIT_RANDOMIZER="$(date|md5sum)" --build-arg SSH_KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)"  --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -t aragno .
+#!/bin/sh
+SOURCE_STAGE="git"
+VALUE=${1:-$SOURCE_STAGE}
+echo $VALUE
+docker build \
+	--build-arg SSH_KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)"  \
+	--build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" \
+	--build-arg GIT_RANDOMIZER="$(date|md5sum)" \
+	--build-arg SOURCE_STAGE=$VALUE \
+	-t aragno .
